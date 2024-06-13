4037f7294ad2d8b9ec6df736cc6ecb6a7f0bcc79
EC2 Default User <ec2-user@ip-172-31-78-22.ec2.internal>
2024-06-13 14:09:55 +0000

    rhrm

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,9 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "familyc2": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
